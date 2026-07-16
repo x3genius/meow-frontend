543b86b43853593aaad8c4aa59e6d797fff344d2
OneTwoZzzPlus <ivansakulin5505@gmail.com>
2026-07-16 22:52:05 +0300

    Change QR

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -170,7 +170,7 @@ export default function Home() {
         </div>
         <div className={styles.qrWrapper}>
           <HashLink to="/help">
-            <img src="help_qr.jpg" alt="Пожертвования" className={styles.qrImage} />
+            <img src="help_qr.png" alt="Пожертвования" className={styles.qrImage} />
           </HashLink>
           <p className={styles.mutedText}>
             Отсканировать можно в приложении любого банка
